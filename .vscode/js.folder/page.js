6f7de31f655fc73212290a791a4c8564de1af500
Mohamed Amine <nafiai.medamine@gmail.com>
2025-11-17 18:45:00 +0100

    varibalbe js

---
+++ .vscode/js.folder/page.js
@@ -0,0 +1,49 @@
+const zoneRules = {
+  Réceptionniste: ["reception"],
+  "Technicien IT": ["servers"],
+  "Agent de sécurité": ["security"],
+  Manager: [
+    "conference", "reception", "servers", "security", "staff", "archives",
+  ], 
+  Nettoyage: ["conference", "reception", "servers", "security", "staff"], 
+  Autre: ["conference", "staff", "archives"], 
+};
+
+const zoneCapacity = {
+  conference: 10,
+  reception: 2,
+  servers: 3,
+  security: 4,
+  staff: 15,
+  archives: 5,
+};
+
+const DEFAULT_PHOTO_URL = "https://tse4.mm.bing.net/th/id/OIP.IGNf7GuQaCqz_RPq5wCkPgHaLH?rs=1&pid=ImgDetMain&o=7&rm=3";
+let storInfoEmploy = []; 
+let isEditMode = false;
+let editingIndex = -1;
+const addEditEmployeeModal = document.getElementById("addEmployesform"); 
+const employeeForm = document.getElementById("employeeForm");
+const container = document.querySelector(".container");
+const fermerForme = document.getElementById("annulerinfo");
+const ajouteEmployersBtn = document.getElementById("addEmployeeBtn");
+
+const nameInput = document.getElementById("nom");
+const roleSelect = document.getElementById("selectAj");
+const imgInput = document.getElementById("img");
+const emailInput = document.getElementById("email");
+const telInput = document.getElementById("tel");
+
+const photoPreviewDiv = document.getElementById("photoPreview");
+const affEmployeers = document.getElementById("unassignedList");
+const paraAssignie = document.getElementById("noUnassignedMsg");
+const profileModal = document.getElementById("profileModal");
+
+const experiencesList = document.getElementById("ajoutExper");
+const addExperienceBtn = document.getElementById("addExperienceBtn");
+
+const assignModal = document.getElementById("assignModal");
+const assignList = document.getElementById("assignList");
+const closeAssignModal = document.getElementById("closeAssignModal");
+const assignZoneTitle = document.getElementById("assignZoneTitle");
+
